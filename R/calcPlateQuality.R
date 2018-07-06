ca041a0e62cc0e1d734ddd8afa16e9c692255ec5
dmattek <macdobry@gmail.com>
2018-07-06 14:27:23 +0200

    Bug fixes

--- R/calcPlateQuality.R
+++ R/calcPlateQuality.R
@@ -7,10 +7,10 @@
 #' Has to consist of at least 2 columns: value of the measurement in a well,
 #' description of well types (e.g.\code{'CTRL neg'}, \code{'CTRL pos'}, \code{'Compounds'}.)
 #'
-#' @param in.col.meas String with column name of the measurement
-#' @param in.col.type String with column name of the well type
+#' @param in.col.meas Name of the column holding the measurement
+#' @param in.col.type Name of the column holding the well type
 #' @param in.well.type Named list with strings that describe wells
-#' with negaitve and positive controls, and compounds. The list has to have the following
+#' with negative and positive controls, and compounds. The list has to have the following
 #' names of elements: \code{list(ctrl.neg = 'CTRL neg', ctrl.pos = 'CTRL pos', compounds = 'Compounds')}
 #' @param in.screen String with either of \code{c('activation', 'inhibition')}
 #'
